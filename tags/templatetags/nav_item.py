6bee116d0e36339eabc8ed679da172e582cab3b7
Kirill Lashuk <kl7.666@gmail.com>
2010-10-31 19:29:32 +0200

    Switch to "nav-active" CSS class.

--- tags/templatetags/nav_item.py
+++ tags/templatetags/nav_item.py
@@ -6,6 +6,6 @@ register = template.Library()
 @register.simple_tag
 def nav_item(request, title, link):
     if request.path == link:
-        return '<span class="nav-item">%s</span>' % title
+        return '<span class="nav-active">%s</span>' % title
     else:
         return '<a href="%s">%s</a>' % (link, title)
